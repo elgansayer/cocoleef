0efe06bc9af30a63610e9166c24f2d47ed5cace3
Elgan <elgan.sayer@gmail.com>
2017-11-10 07:52:24 +0000

    couple of linux make files

---
+++ linux-build/CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elgan/mobile-interview-task/ElgGame/Classes/AppDelegate.cpp" "/home/elgan/mobile-interview-task/ElgGame/linux-build/CMakeFiles/MyGame.dir/Classes/AppDelegate.cpp.o"
+  "/home/elgan/mobile-interview-task/ElgGame/Classes/HelloWorldScene.cpp" "/home/elgan/mobile-interview-task/ElgGame/linux-build/CMakeFiles/MyGame.dir/Classes/HelloWorldScene.cpp.o"
+  "/home/elgan/mobile-interview-task/ElgGame/proj.linux/main.cpp" "/home/elgan/mobile-interview-task/ElgGame/linux-build/CMakeFiles/MyGame.dir/proj.linux/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COCOS2D_DEBUG=1"
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/GLFW"
+  "/usr/include/GLFW"
+  "../cocos2d"
+  "../cocos2d/cocos"
+  "../cocos2d/external"
+  "../cocos2d/cocos/platform"
+  "../cocos2d/cocos/audio/include"
+  "../Classes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/cocos/CMakeFiles/cocos2d.dir/DependInfo.cmake"
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/cocos/CMakeFiles/cocos2dInternal.dir/DependInfo.cmake"
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/external/unzip/CMakeFiles/unzip.dir/DependInfo.cmake"
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/external/tinyxml2/CMakeFiles/tinyxml2.dir/DependInfo.cmake"
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/external/xxhash/CMakeFiles/xxhash.dir/DependInfo.cmake"
+  "/home/elgan/mobile-interview-task/ElgGame/linux-build/cocos2d/external/recast/CMakeFiles/recast.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
